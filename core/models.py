bf22156e67b7dfde3adf602636cd9be533df1172
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-05 08:34:27 +0200

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
