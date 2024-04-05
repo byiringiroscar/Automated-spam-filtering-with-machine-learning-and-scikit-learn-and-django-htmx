18043c84bb66bb74da86d2718d59addab44061d4
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-05 08:37:33 +0200

    wip on comments

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from core.models import Comment
 
 # Create your views here.
+
+
+
+def comments(request):
+    comments = Comment.objects.all()
+    context = {
+        'comments': comments
+    }
+    return render(request, 'comments.html', context=)
+
