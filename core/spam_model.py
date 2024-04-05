357630c423ab8f51a11c09514238eca2f804cdea
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-05 09:05:19 +0200

    Update spam_model.py

--- core/spam_model.py
+++ core/spam_model.py
@@ -7,3 +7,15 @@ from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 
 KNN_MODE_FILE = settings.BASE_DIR / 'data' / 'spam_model.joblib'
 VECTORIZER_FILE = settings.BASE_DIR / 'data' / 'tfidf.joblib'
+
+knn = load(KNN_MODE_FILE)
+tfidf = load(VECTORIZER_FILE)
+
+def preprocess(text):
+  # remove punctuation and lowercase
+  text = "".join([t.lower() for t in text if t not in string.punctuation])
+  # tokenize
+  tokens = text.split(" ") # 'color printing' == ['color', 'printing']
+
+  # filter out stopwords
+  return " ".join(t for t in tokens if t not in ENGLISH_STOP_WORDS)
