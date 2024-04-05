b564e9d3a55a308d6ba8c6b1cfd3af9805c4c33c
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-05 09:55:50 +0200

    Add detect colors and func in views

--- core/spam_model.py
+++ core/spam_model.py
@@ -1,21 +1,32 @@
+import string 
 from django.conf import settings
-import string
 
 from joblib import load
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 
 
-KNN_MODE_FILE = settings.BASE_DIR / 'data' / 'spam_model.joblib'
+KNN_MODEL_FILE = settings.BASE_DIR / 'data' / 'spam_model.joblib'
 VECTORIZER_FILE = settings.BASE_DIR / 'data' / 'tfidf.joblib'
 
-knn = load(KNN_MODE_FILE)
 tfidf = load(VECTORIZER_FILE)
+knn = load(KNN_MODEL_FILE)
 
 def preprocess(text):
-  # remove punctuation and lowercase
-  text = "".join([t.lower() for t in text if t not in string.punctuation])
-  # tokenize
-  tokens = text.split(" ") # 'color printing' == ['color', 'printing']
+    # remove punctuation and lowercase
+    text = "".join([t.lower() for t in text if t not in string.punctuation])
+    
+    # tokenize
+    tokens = text.split(" ")
+    
+    # remove stopwords
+    return " ".join(t for t in tokens if t not in ENGLISH_STOP_WORDS)
 
-  # filter out stopwords
-  return " ".join(t for t in tokens if t not in ENGLISH_STOP_WORDS)
+def predict_spam(text):
+    # process each "document" (i.e. each comment)
+    processed = [preprocess(t) for t in text]
+
+    # create vectors
+    vectors = tfidf.transform(processed)
+
+    # get spam prediction
+    return knn.predict(vectors)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from core.models import Comment
+from core.spam_model import predict_spam
 
 # Create your views here.
 
@@ -15,8 +16,9 @@ def comments(request):
 
 def check_spam(request):
     comments = Comment.objects.all()
+    predictions = predict_spam(comments.values_list('text', flat=True))
     context = {
-        'comments': comments
+        'comments': zip(comments, predictions)
     }
     return render(request, 'partials/comments-spam.html', context)
 
